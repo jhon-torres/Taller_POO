ddc1331d35f7b1ff696e4c0d075ade21dbb16914
jhon-torres <jhon-3316@hotmail.com>
2022-07-05 11:38:58 -0500

    Clase enteroLong
    Clase enteroLong constructor y setters y getters y cambio mensaje en BYteShort

--- src/Byte_Short.java
+++ src/Byte_Short.java
@@ -31,7 +31,9 @@ public class Byte_Short {
 
     @Override
     public String toString() {
-    return "Valor de byte: "+var_byte+"\n"+
+
+    return  "Pasar de byte a Short: "+var_byte+"\n"+
+            "Valor de byte: "+var_byte+"\n"+
             "Valor de short: "+var_short;
     }
 }
